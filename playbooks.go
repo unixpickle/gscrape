734a2a48a046352e8eeaa96eadb5706daf2990a9
Alex Nichol <unixpickle@gmail.com>
2016-01-08 11:18:41 -0500

    MyBooks() API is now pretty nice

--- playbooks.go
+++ playbooks.go
@@ -1,14 +1,43 @@
 package gscrape
 
 import (
+	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
 var niceUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:40.0) Gecko/20100101 " +
 	"Firefox/40.0"
+
+type BookSource int
+
+const (
+	BookSourcePreordered BookSource = iota
+	BookSourcePreviouslyRented
+	BookSourcePublicDomain
+	BookSourcePurchased
+	BookSourceRented
+	BookSourceSample
+	BookSourceUploaded
+)
+
+var AllBookSources = []BookSource{
+	BookSourcePreordered,
+	BookSourcePreviouslyRented,
+	BookSourcePublicDomain,
+	BookSourcePurchased,
+	BookSourceRented,
+	BookSourceSample,
+	BookSourceUploaded,
+}
+
+type BookInfo struct {
+	Title   string   `json:"title"`
+	Authors []string `json:"authors"`
+}
 
 // AuthPlayBooks is a wrapper for Authenticate() that uses Google Play Books.
 func (s *Session) AuthPlayBooks(email, password string) error {
@@ -28,28 +57,86 @@ func NewPlayBooks(s *Session) (*PlayBooks, error) {
 	return &PlayBooks{s, *info}, nil
 }
 
-// MyBooks fetches the raw "mybooks" API.
-// TODO: support pagination, parsing, and filter options.
-func (p *PlayBooks) MyBooks() (string, error) {
+type volumeObject struct {
+	VolumeInfo BookInfo `json:"volumeInfo"`
+}
+
+type booksResponse struct {
+	Items      []volumeObject `json:"items"`
+	TotalItems int            `json:"totalItems"`
+}
+
+// MyBooks fetches the user's books, filtering by source.
+//
+// You must read the returned book channel all the way to the end,
+// even if you are not interested in all the books.
+//
+// If an error occurs asynchronously, it will be sent on the error channel.
+//
+// Both returned channels will be closed once the asynchronous operation is complete.
+func (p *PlayBooks) MyBooks(sources []BookSource) (<-chan BookInfo, <-chan error) {
 	u := "https://clients6.google.com/books/v1/volumes/mybooks?" +
-		"acquireMethod=PREORDERED&acquireMethod=PREVIOUSLY_RENTED&acquireMethod=PUBLIC_DOMAIN&" +
-		"acquireMethod=PURCHASED&acquireMethod=RENTED&acquireMethod=SAMPLE&" +
-		"acquireMethod=UPLOADED&maxResults=40&source=ge-books-fe&startIndex=0&key=" +
-		p.info.requestKey
-	req, _ := http.NewRequest("GET", u, nil)
-	req.Header.Add("Host", "clients6.google.com")
-	req.Header.Add("OriginToken", p.info.originToken)
-	req.Header.Add("X-Origin", "https://play.google.com")
-	resp, err := p.s.Do(req)
-	if err != nil {
-		return "", err
+		"maxResults=40&source=ge-books-fe&key=" + p.info.requestKey
+	for _, source := range sources {
+		u += "&acquireMethod="
+		switch source {
+		case BookSourcePreordered:
+			u += "PREORDERED"
+		case BookSourcePreviouslyRented:
+			u += "PREVIOUSLY_RENTED"
+		case BookSourcePublicDomain:
+			u += "PUBLIC_DOMAIN"
+		case BookSourcePurchased:
+			u += "PURCHASED"
+		case BookSourceRented:
+			u += "RENTED"
+		case BookSourceSample:
+			u += "SAMPLE"
+		case BookSourceUploaded:
+			u += "UPLOADED"
+		default:
+			panic("unknown book source: " + strconv.Itoa(int(source)))
+		}
 	}
-	defer resp.Body.Close()
-	contents, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(contents), nil
+
+	errChan := make(chan error, 1)
+	bookChan := make(chan BookInfo)
+
+	go func() {
+		defer close(bookChan)
+		defer close(errChan)
+
+		i := 0
+		for {
+			useURL := u
+			useURL += "&startIndex=" + strconv.Itoa(i)
+			req, _ := http.NewRequest("GET", useURL, nil)
+			req.Header.Add("Host", "clients6.google.com")
+			req.Header.Add("OriginToken", p.info.originToken)
+			req.Header.Add("X-Origin", "https://play.google.com")
+			resp, err := p.s.Do(req)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			contents, err := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+			var fullResponse booksResponse
+			if err := json.Unmarshal(contents, &fullResponse); err != nil {
+				errChan <- err
+				return
+			}
+			for _, volume := range fullResponse.Items {
+				bookChan <- volume.VolumeInfo
+			}
+			i += len(fullResponse.Items)
+			if i >= fullResponse.TotalItems {
+				return
+			}
+		}
+	}()
+
+	return bookChan, errChan
 }
 
 // playBooksAuthInfo stores extra authentication information needed
