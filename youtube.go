ebd24306665baac53980d1875f09d3cb707508fe
henkman <henkman@github.com>
2016-05-05 21:53:41 +0200

    yt history crawler fully functional

--- youtube.go
+++ youtube.go
@@ -1,11 +1,12 @@
 package gscrape
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
-	"github.com/yhat/scrape"
-	"golang.org/x/net/html"
-	"golang.org/x/net/html/atom"
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Video struct {
@@ -17,58 +18,106 @@ type Video struct {
 	Length      string
 }
 
-type YTWatchHistory struct {
-	Videos []Video
-}
-
-func (s *Session) YTWatchHistory(email, password string) (*YTWatchHistory, error) {
-	if err := s.Auth("https://accounts.google.com/ServiceLogin?service=youtube", email, password); err != nil {
+// Returns watch history.
+// In case of an error it returns the gathered videos up to that point.
+// The time it takes to get the history depends on the number of watched videos.
+// It might take a long time. I warned you.
+func (s *Session) YTWatchHistory(email, password string) ([]Video, error) {
+	if err := s.Auth("https://accounts.google.com/ServiceLogin?service=youtube",
+		email, password); err != nil {
 		return nil, err
+	}
+	{ // this is required to get some cookies
+		req, _ := http.NewRequest("GET",
+			"https://www.youtube.com/feed/subscriptions", nil)
+		req.Header.Set("User-Agent", niceUserAgent)
+		resp, err := s.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		resp.Body.Close()
 	}
 	return s.getWatchHistory()
 }
 
-func (s *Session) getWatchHistory() (*YTWatchHistory, error) {
-	req, _ := http.NewRequest("GET", "https://www.youtube.com/feed/subscriptions", nil)
-	req.Header.Set("User-Agent", niceUserAgent)
-	resp, err := s.Do(req)
-	if err != nil {
-		return nil, err
+func (s *Session) getWatchHistory() ([]Video, error) {
+	videos := make([]Video, 0, 128)
+	getVideos := func(doc *goquery.Document) {
+		doc.Find(".yt-lockup-video").Each(
+			func(i int, s *goquery.Selection) {
+				var video Video
+				video.Id, _ = s.Attr("data-context-item-id")
+				video.Thumbnail, _ = s.Find("img").Attr("src")
+				video.Length = s.Find(".video-time").Text()
+				video.Title, _ = s.Find(".yt-lockup-title a").Attr("title")
+				video.Author = s.Find(".yt-lockup-byline a").Text()
+				dn := s.Find(".yt-lockup-description")
+				if dn.Length() > 0 {
+					video.Description = dn.Text()
+				}
+				videos = append(videos, video)
+			})
 	}
-	defer resp.Body.Close()
-
-	videos := make([]Video, 0, 100)
-	{ // first page is in response body
-		parsed, err := html.Parse(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-		videoNodes := scrape.FindAll(parsed, scrape.ByClass("yt-lockup-video"))
-		for _, videoNode := range videoNodes {
-			var video Video
-			video.Id = getAttribute(videoNode, "data-context-item-id")
-			thumbNode, _ := scrape.Find(videoNode, scrape.ByTag(atom.Img))
-			video.Thumbnail = getAttribute(thumbNode, "src")
-			lengthNode, _ := scrape.Find(videoNode, scrape.ByClass("video-time"))
-			video.Length = lengthNode.FirstChild.Data
-			titleNode, _ := scrape.Find(videoNode, scrape.ByClass("yt-lockup-title"))
-			linkNode, _ := scrape.Find(titleNode, scrape.ByTag(atom.A))
-			video.Title = getAttribute(linkNode, "title")
-			descriptionNode, _ := scrape.Find(videoNode, scrape.ByClass("yt-lockup-description"))
-			if descriptionNode != nil {
-				video.Description = descriptionNode.FirstChild.Data
+	var more string
+	var hasMore bool
+	{ // the first few videos are directly on the page
+		var doc *goquery.Document
+		{
+			req, err := http.NewRequest("GET",
+				"https://www.youtube.com/feed/history", nil)
+			req.Header.Set("User-Agent", niceUserAgent)
+			resp, err := s.Do(req)
+			if err != nil {
+				return videos, err
 			}
-			byLineNode, _ := scrape.Find(videoNode, scrape.ByClass("yt-lockup-byline"))
-			authorNode, _ := scrape.Find(byLineNode, scrape.ByTag(atom.A))
-			video.Author = authorNode.FirstChild.Data
-			videos = append(videos, video)
+			doc, err = goquery.NewDocumentFromResponse(resp)
+			if err != nil {
+				resp.Body.Close()
+				return videos, err
+			}
+			resp.Body.Close()
+		}
+		getVideos(doc)
+		mn := doc.Find("[data-uix-load-more-href]")
+		more, hasMore = mn.Attr("data-uix-load-more-href")
+	}
+	// all the other videos need to be gathered through ajax loaded json
+	for hasMore {
+		var yth struct {
+			ContentHTML        string `json:"content_html"`
+			LoadMoreWidgetHTML string `json:"load_more_widget_html"`
+		}
+		{
+			url_str := fmt.Sprint("https://www.youtube.com", more)
+			req, err := http.NewRequest("GET", url_str, nil)
+			req.Header.Set("User-Agent", niceUserAgent)
+			resp, err := s.Do(req)
+			if err != nil {
+				return videos, err
+			}
+			if err := json.NewDecoder(resp.Body).Decode(&yth); err != nil {
+				resp.Body.Close()
+				return videos, err
+			}
+			resp.Body.Close()
+		}
+		{
+			doc, err := goquery.NewDocumentFromReader(
+				bytes.NewBufferString(yth.ContentHTML))
+			if err != nil {
+				return videos, err
+			}
+			getVideos(doc)
+		}
+		{
+			doc, err := goquery.NewDocumentFromReader(
+				bytes.NewBufferString(yth.LoadMoreWidgetHTML))
+			if err != nil {
+				return videos, err
+			}
+			mn := doc.Find("[data-uix-load-more-href]")
+			more, hasMore = mn.Attr("data-uix-load-more-href")
 		}
 	}
-	{ // further pages using ajax
-
-	}
-
-	wh := new(YTWatchHistory)
-	wh.Videos = videos
-	return wh, nil
+	return videos, nil
 }
